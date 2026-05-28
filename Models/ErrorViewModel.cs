354bd43ccc14337979f2aed08207c73796daa020
GABRIEL MARTINEZ LOUFER <49960445@ort>
2026-05-28 08:16:41 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP03_Martinez_Loufer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/PalabrasAhorcado.cs
@@ -0,0 +1,10 @@
+namespace TP03_Martinez_Loufer.Models;
+
+public class PalabrasAhorcado{
+    private List<string> palabras;
+
+    public PalabrasAhorcado(){
+        palabras = new List(){"petricor", "ataraxia", "megalovania", "arrebol", "melifluo", "afrodita", "resiliencia", "yacimiento", "nihilismo", "ferviente"}
+    }
+    
+}
