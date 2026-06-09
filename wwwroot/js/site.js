871f823e8ccb6c006dad567b2b5aaa584d70f13a
GABRIEL MARTINEZ LOUFER <49960445@ort>
2026-06-09 08:52:00 -0300

    extras

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,8 +4,15 @@
     let palabraOculta = document.getElementById("palabraOculta").value;
     let mostrarPalabra = document.getElementById("mostrarPalabra");
     let resultado = document.getElementById("resultado");
+    let letrasUsadas = document.getElementById("letrasUsadas");
+    resultado.innerHTML = "";
 
-    if(cantIntentos.innerText >0 && mostrarPalabra.innerText != palabraOculta.toUpperCase()){
+    if(letrasUsadas.value.includes(letra)){
+        resultado.innerHTML = "Ya usaste esa letra";
+        resultado.style.color = "yellow";
+    }
+    
+    if(cantIntentos.innerText > 0 && mostrarPalabra.innerText != palabraOculta.toUpperCase() && !letrasUsadas.value.includes(letra)){
         if(palabraOculta.includes(letra)){
             let palabraintermedia = "";
             for(let i = 0; i < palabraOculta.length; i++){
@@ -21,15 +28,18 @@
         else{
             cantIntentos.innerText -= 1;
         }
+        letrasUsadas.value += letra.toUpperCase() + " ";
     }
     
     
     if(mostrarPalabra.innerText == palabraOculta.toUpperCase()){
-        resultado.innerHTML = "Ganaste!!!!! felicidades por tu victoria"
-        resultado.style.color = "green"
+        resultado.innerHTML = "Ganaste!!!!! felicidades por tu victoria";
+        resultado.style.color = "green";
     }
+
     if(cantIntentos.innerText == 0){
-        resultado.innerHTML = "Perdiste, suerte la proxima!"
-        resultado.style.color = "red"
+        resultado.innerHTML = "Perdiste, suerte la proxima!";
+        resultado.style.color = "red";
     }
+    
 }
